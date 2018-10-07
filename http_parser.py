9fcbd3b3d78a5d3c7cef46c90ddba3978f849a0d
Han <hanyourmd@gmail.com>
2018-10-07 16:55:15 -0600

    moving things around

--- http_parser.py
+++ http_parser.py
@@ -45,7 +45,6 @@ class HTTPParseState(Enum):
     PARTIAL = 1
     """Incomplete, waiting for more."""
     ERROR = 2
-    SUCCESS = 3
 
 
 class HTTPBodyEncoding(Enum):
@@ -55,26 +54,54 @@ class HTTPBodyEncoding(Enum):
 
 class HTTPBodyParser:
     def __init__(self, header: HTTPRequestHeader):
+        self._buf = b""
         raise NotImplementedError()
 
-    def feed(self, s: bytes)->int:
+    def feed(self, s: bytes)->Union[HTTPParseState, int]:
         """
-        Feed a string to the parser. :return HTTPParseState.PARTIAL if more are expected; HTTPParseState.ERROR if an
-        error occured; an integer n if only n characters are consumed, and the rest belongs to the next request.
+        Feed a byte string to the parser.
+        :return HTTPParseState.PARTIAL if more are expected; HTTPParseState.ERROR if an error occured; an integer n if only n characters are consumed, and the rest belongs to the next request.
+        :raises RuntimeError when called after a result has been returned.
         """
         raise NotImplementedError()
+
+class _HeaderParserState(Enum):
+    # Waiting for H
+    H = 1
+    # Had H, waiting for T
+    HT = 2
+    HTT = 3
+    HTTP = 4
+    HTTP_space = 5
+    # Had HTTP_, waiting for url; or in url already, waiting for more url
+    HTTP_space_url = 6
+    # Transition when get a space
+    # Had a space, waiting for version; or in version already, waiting for more version
+    HTTP_space_url_space_version = 7
+    # Transition when get a \r
+    first_line_rn = 8
+    # Transition when get a \n
+
+    # loop
+    # Had an ending \r, waiting for \n
+    ending_r = 9
 
 
 class HTTPHeaderParser:
     def __init__(self):
+        self._buf = b""
         self._parse_result = None
         self._result_ready = False
 
-    def feed(self, s: Ref)->Union[HTTPParseState, HTTPLocation]:
+    def feed(self, s: Ref)->Union[HTTPParseState, HTTPRequestHeader]:
         """
-        Feed the reference of a string into the parser for parsing. After this methods complete, if there are
+        Feed the reference of a byte string into the parser for parsing. After this methods complete, if there are
         unconsumed characters, they'll be stored back into s.
         :param s: Reference of a string to be fed into the parser.
+        :raises RuntimeError when called after has already been returned.
         :return: State of the parsing afterward, or the result.
         """
+        self._buf += s.v
+        while self
+        one_of("HTTP ")
         raise NotImplementedError()
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from typing import Generic
 
 
-class Ref:
+class Ref(Generic[T]):
     """
     Pass statis values (str, int, etc) by reference.
     """
-    def __init__(self, v):
-        self.v = v
+    def __init__(self, v: T):
+        self.v: T = v
