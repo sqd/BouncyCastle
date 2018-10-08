a93b9c4f10e3b98a6d1d0b74472014316fc7d032
Han <hanyourmd@gmail.com>
2018-10-07 20:52:11 -0600

    HTTP parser

--- http_parser.py
+++ http_parser.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from enum import Enum
+from enum import Enum, auto
 from typing import List, Tuple, Union
+from string import ascii_letters, digits, punctuation
 
 from utils import Ref
+
+
+# https://tools.ietf.org/html/rfc2616#page-17
+_token = ascii_letters + digits + "-!#$%&'*+-.^_`|~"
+
+_uri = ascii_letters + digits + punctuation
 
 
 class HTTPLocation:
@@ -41,15 +48,16 @@ class HTTPRequestHeader:
         raise NotImplementedError()
 
 
-class HTTPParseState(Enum):
-    PARTIAL = 1
+class HTTPParseStatus(Enum):
+    PARTIAL = auto()
     """Incomplete, waiting for more."""
-    ERROR = 2
+    ERROR = auto()
+    SUCCEED = auto()
 
 
 class HTTPBodyEncoding(Enum):
-    CONTENT_LENGTH = 1
-    CHUNKED = 2
+    CONTENT_LENGTH = auto()
+    CHUNKED = auto()
 
 
 class HTTPBodyParser:
@@ -57,51 +65,221 @@ class HTTPBodyParser:
         self._buf = b""
         raise NotImplementedError()
 
-    def feed(self, s: bytes)->Union[HTTPParseState, int]:
+    def feed(self, s: bytes)->Union[HTTPParseStatus, int]:
         """
         Feed a byte string to the parser.
-        :return HTTPParseState.PARTIAL if more are expected; HTTPParseState.ERROR if an error occured; an integer n if only n characters are consumed, and the rest belongs to the next request.
+        :return HTTPParseStatus.PARTIAL if more are expected; HTTPParseStatus.ERROR if an error occured; an integer n if only n characters are consumed, and the rest belongs to the next request.
         :raises RuntimeError when called after a result has been returned.
         """
         raise NotImplementedError()
 
-class _HeaderParserState(Enum):
-    # Waiting for H
-    H = 1
-    # Had H, waiting for T
-    HT = 2
-    HTT = 3
-    HTTP = 4
-    HTTP_space = 5
-    # Had HTTP_, waiting for url; or in url already, waiting for more url
-    HTTP_space_url = 6
-    # Transition when get a space
-    # Had a space, waiting for version; or in version already, waiting for more version
-    HTTP_space_url_space_version = 7
-    # Transition when get a \r
-    first_line_rn = 8
-    # Transition when get a \n
 
-    # loop
-    # Had an ending \r, waiting for \n
-    ending_r = 9
+class HTTPParseFailedError(Exception):
+    """Thrown when trying to feed more value into a HTTP parser when it has failed."""
+    pass
+
+
+class HTTPParseReturnedAlreadyError(Exception):
+    """Thrown when trying to feed more value into a HTTP parser when it has returned already."""
+    pass
 
 
 class HTTPHeaderParser:
     def __init__(self):
         self._buf = b""
-        self._parse_result = None
-        self._result_ready = False
+        self._parse_result = HTTPRequestHeader()
+        self._parse_status = HTTPParseStatus.PARTIAL
+        self._parse_func = self._start
 
-    def feed(self, s: Ref)->Union[HTTPParseState, HTTPRequestHeader]:
+    def _post_process(self):
+        """
+        Post process self._parse_result:
+        1. Parse location
+        2. change headers to tuples
+        """
+        raise NotImplementedError()
+
+    def feed(self, s: Ref[bytes])->Union[HTTPParseStatus, HTTPRequestHeader]:
         """
         Feed the reference of a byte string into the parser for parsing. After this methods complete, if there are
         unconsumed characters, they'll be stored back into s.
         :param s: Reference of a string to be fed into the parser.
-        :raises RuntimeError when called after has already been returned.
+        :raises HTTPParseReturnedAlreadyError when called after a result has already been returned.
+        :raises HTTPParseFailedError when called after parsing has failed.
         :return: State of the parsing afterward, or the result.
         """
-        self._buf += s.v
-        while self
-        one_of("HTTP ")
-        raise NotImplementedError()
+        if self._parse_status == HTTPParseStatus.SUCCEED:
+            raise HTTPParseReturnedAlreadyError()
+        elif self._parse_status == HTTPParseStatus.ERROR:
+            raise HTTPParseFailedError()
+        i = 0
+        while i < len(s):
+            c = s[i]
+            self._parse_result.consumed += c
+            result = self._parse_func(c)
+            if result == HTTPParseStatus.ERROR:
+                self._parse_status = result
+                return result
+            elif result == HTTPParseStatus.SUCCEED:
+                self._parse_status = result
+                self._post_process()
+                s.v = s.v[i+1:]
+                return self._parse_result
+        s.v = b""
+        return HTTPParseStatus.PARTIAL
+
+    def _start(self, c:str):
+        if c == '\r':
+            self._parse_func = self._prefix_r
+        elif c in _token:
+            self._parse_func = self._method
+            self._parse_result.method = c
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _prefix_r(self, c:str):
+        if c == '\n':
+            self._parse_func = self._start
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _method(self, c:str):
+        if c in _token:
+            self._parse_result.method += c
+        elif c == ' ':
+            self._parse_func = self._method_
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _method_(self, c:str):
+        if c in _uri:
+            self._parse_result.uri += c
+            self._parse_func = self._m_uri
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _m_uri(self, c:str):
+        if c in _uri:
+            self._parse_result.uri += c
+        elif c == ' ':
+            self._parse_func = self._m_uri_
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _m_uri_(self, c:str):
+        if c == 'H':
+            self._parse_result.version = c
+            self._parse_func = self._m_url_h
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _m_url_h(self, c:str):
+        if c == 'T':
+            self._parse_result.version += c
+            self._parse_func = self._m_url_ht
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _m_url_ht(self, c:str):
+        if c == 'T':
+            self._parse_result.version += c
+            self._parse_func = self._m_url_htt
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _m_url_htt(self, c:str):
+        if c == 'P':
+            self._parse_result.version += c
+            self._parse_func = self._m_url_http
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _m_url_http(self, c:str):
+        if c == '/':
+            self._parse_result.version += c
+            self._parse_func = self._m_url_https
+        else:
+            return HTTPParseStatus.ERROR
+
+    # s = slash
+    def _m_url_https(self, c:str):
+        if c.isdigit():
+            self._parse_result.version += c
+            self._parse_func = self._m_url_httpsd
+        else:
+            return HTTPParseStatus.ERROR
+
+    # d1 = digit
+    def _m_url_httpsd(self, c:str):
+        if c == '.':
+            self._parse_result.version += c
+            self._parse_func = self._m_url_httpsdd
+        else:
+            return HTTPParseStatus.ERROR
+
+    # d1 = digit, d2 = dot
+    def _m_url_httpsdd(self, c:str):
+        if c.isdigit():
+            self._parse_result.version += c
+            self._parse_func = self._m_url_httpsddd
+        else:
+            return HTTPParseStatus.ERROR
+
+    # d1 = digit, d2 = dot, d3 = digit
+    def _m_url_httpsddd(self, c:str):
+        if c == '\r':
+            self._parse_func = self._line_r
+        else:
+            return HTTPParseStatus.ERROR
+
+    # Got an non-ending \r
+    def _line_r(self, c:str):
+        if c == '\n':
+            self._parse_func = self._line_rn
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _line_rn(self, c:str):
+        if c in _token:
+            self._parse_func = self._header_key
+            self._parse_result.headers.append([c, ""])
+        elif c == '\r':
+            self._parse_func = self._end_r
+        else:
+            return HTTPParseStatus.ERROR
+
+    def _header_key(self, c:str):
+        if c == ':':
+            self._parse_func = self._header_key_c
+        elif c in _token:
+            self._parse_result.headers[-1][0] += c
+        else:
+            return HTTPParseStatus.ERROR
+
+    # c = colon
+    def _header_key_c(self, c:str):
+        if c == ' ':
+            pass
+        elif c in "\r\n":
+            return HTTPParseStatus.ERROR
+        else:
+            # not completely compliant but fuck the messed up HTTP standards
+            self._parse_result.headers[-1][1] += c
+            self._parse_func = self._header_key_c_val
+
+    def _header_key_c_val(self, c:str):
+        if c == '\r':
+            self._parse_func = self.line_r
+        else:
+            self._parse_result.headers[-1][1] += c
+
+    # Got an ending \r
+    def _end_r(self, c:str):
+        if c == '\n':
+            self._parse_func = self._end_rn
+            return HTTPParseStatus.SUCCEED
+        else:
+            return HTTPParseStatus.ERROR
+
+    def self._endrn(self, c:str):
+        return HTTPParseStatus.ERROR
