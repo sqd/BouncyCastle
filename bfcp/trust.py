a3224bbbddfb8a2308dfeb3d9e6a384bb4e8027b
Mishig Davaadorj <dmishig@gmail.com>
2018-10-11 18:00:55 -0600

    added code to TrustTableManager

--- bfcp/trust.py
+++ bfcp/trust.py
@@ -7,18 +7,30 @@ from protos.bfcp_pb2 import NodeTable, NodeTableEntry, EndNodeRequirement
 
 
 class TrustTableManager:
+    def __init__(self):
+        """ Should contain self.node_table property """
+        self.node_table = None # TODO
+        raise NotImplementedError()
+
     def get_node_table(self) -> NodeTable:
         """
         Gets the node table of this node in the bfcp
         """
-        raise NotImplementedError()
+        return self.node_table
 
     async def update_table(self):
         raise NotImplementedError()
 
     def get_node_by_pubkey(self, pubkey: RsaKey) -> NodeTableEntry:
-        """Returns None if not found"""
-        raise NotImplementedError()
+        for node_table_entry in self.node_table.entries:
+            node = node_table_entry.node
+            # TODO check if node.public_key == pubkey
+            # either convert node.public_key to `RsaKey` OR pubkey to `bytes`
+            pub_key_match = None
+            raise NotImplementedError()
+            if pub_key_match:
+                return node_table_entry
+        return None
 
     def get_pubkey_by_node_requirement(self, en_requirement: EndNodeRequirement) -> RsaKey:
         """ Returns a node public key, given EndNodeRequirement like location must be in China """
