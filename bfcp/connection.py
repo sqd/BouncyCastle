a57af3e57bde5a1433bd288d3d1331c3c82f09b3
sqdyou <hanyourmd@gmail.com>
2018-10-15 14:10:42 -0600

    loop import and fix main.py logging

--- bfcp/connection.py
+++ bfcp/connection.py
@@ -16,8 +16,6 @@ from Crypto.PublicKey.RSA import RsaKey
 
 from bfcp.protocol import pubkey_to_proto, proto_to_pubkey, get_node_pub_key
 from bfcp.messages import TrafficManager, NodeNotFoundError
-from bfcp.node import BFCNode
-from bfcp.trust import TrustTableManager
 import utils
 
 from config import *
