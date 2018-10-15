54339f36c2d2c1289d896d31843ba4a28abc9678
sqdyou <hanyourmd@gmail.com>
2018-10-15 11:38:16 -0600

    HTTP proxy should be able to spin up

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-from bfc import BFCNode
+
+from threading import Thread
+
+from bfcp.node import BFCNode
+
 from event_server import EventServer
 from http_proxy import HTTPProxyServer
 from config import HTTPProxyServerConfig
@@ -11,8 +15,11 @@ _log = getLogger(__name__)
 def main():
     http_proxy_default_config = HTTPProxyServerConfig([("127.0.0.1", 8080)])
 
-    bfc = None # BFCNode(config)
+    bfc = BFCNode(("0.0.0.0", 9000), )
+    Thread(target=bfc.run).start()
+
     ev_server = EventServer()
+
     http_proxy = HTTPProxyServer(http_proxy_default_config, bfc, ev_server)
     http_proxy.start()
 
